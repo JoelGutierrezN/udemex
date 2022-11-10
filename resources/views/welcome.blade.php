b09c470939e476fafcd5ebdd536f7159a56cf9f7
Gutierrez Engombia Jesus Samael <al221911279@gmail.com>
2022-11-10 10:04:33 -0600

    Pestaña de Datos personales del formulario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,24 +13,65 @@
             <button type="button" data-tab-target="2">Datos de la Carrera Profesional</button>
         </div>
 
+        {{-- Datos personales --}}
         <div class="mt-2" data-tab-id="1">
             <h3>Datos Personales</h3>
             <div class="input-columns-2">
                 <div>
-                    <label for="text-input">Nombre del campo</label>
-                    <input type="text" placeholder="Este es un placeholder" id="text-input">
+                    <label for="text-input">Nombres</label>
+                    <input type="text" placeholder="Coloque su nombre" id="text-input">
                 </div>
 
                 <div>
-                    <label for="select-input">Nombre del campo</label>
+                    <label for="text-input">Apellido Paterno</label>
+                    <input type="text" placeholder="Coloque su apellido paterno" id="text-input">
+                </div>
+
+                <div>
+                    <label for="text-input">Apellido Materno</label>
+                    <input type="text" placeholder="Coloque su apellido materno" id="text-input">
+                </div>
+
+                <div>
+                    <label for="text-input">Número de empleado EDEMEX</label>
+                    <input type="text" placeholder="Número de empleado EDEMEX" id="text-input">
+                </div>
+
+                <div>
+                    <label for="text-input">Telefono de casa</label>
+                    <input type="text" placeholder="Coloque su telefono de casa" id="text-input">
+                </div>
+
+                <div>
+                    <label for="text-input">Telefono Celular</label>
+                    <input type="text" placeholder="Coloque su telefono celular" id="text-input">
+                </div>
+
+                <div>
+                    <label for="text-input">Email de EDEMEX</label>
+                    <input type="text" placeholder="Coloque su email de EDEMEX" id="text-input">
+                </div>
+
+                <div>
+                    <label for="text-input">Email Personal</label>
+                    <input type="text" placeholder="Coloque su email personal" id="text-input">
+                </div>
+
+                <div>
+                    <label for="select-input">Rol</label>
                     <select id="select-input">
-                        <option value="">opcion 1</option>
-                        <option value="">opcion 2</option>
-                        <option value="">opcion 3</option>
+                        <option value="">Docente</option>
+                        <option value="">Asistente</option>
                     </select>
+                </div>
+
+                <div>
+                    <label for="text-input">Fotografia</label>
+                    <input type="file" placeholder="Coloque su fotografia" id="text-input">
                 </div>
             </div>
         </div>
+        {{--Fin Datos personales --}}
 
         <div class="mt-2" data-tab-id="2">
             <h3>Datos de la Carrera Profesional</h3>
@@ -39,7 +80,6 @@
                     <label for="text-input-2">Nombre del campo 2</label>
                     <input type="text" placeholder="Este es un placeholder" id="text-input-2">
                 </div>
-
                 <div>
                     <label for="select-input-2">Nombre del campo 2</label>
                     <select id="select-input-2">
@@ -50,7 +90,6 @@
                 </div>
             </div>
         </div>
-
 
     </form>
 
