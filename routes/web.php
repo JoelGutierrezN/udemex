f8346cbc857fd7b9123f834e9d9cf2519f9fb173
Joel Gutierrez Nuñez <65819479+JoelGutierrezN@users.noreply.github.com>
2022-11-09 21:47:23 -0600

    Avance Auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::redirect('/', 'login');
+Route::group(['middleware' => ['web', 'guest'], 'namespace' => 'App\Http\Controllers\Auth'], function(){
+    Route::get('login', 'AuthController@login')->name('login');
+    Route::get('connect', 'AuthController@connect')->name('connect');
+});
+
+Route::group(['middleware' => ['web', 'MsGraphAuthenticated'], 'prefix' => 'app', 'namespace' => 'App\Http\Controllers'], function(){
+    Route::get('/', 'PagesController@app')->name('app');
+    Route::get('logout', 'Auth\AuthController@logout')->name('logout');
+});
