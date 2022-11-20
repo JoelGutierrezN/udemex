0d6b9229edb8f98a8bf192fb131dfecfb3ba51e0
Joel Gutierrez Nuñez <65819479+JoelGutierrezN@users.noreply.github.com>
2022-11-20 13:38:19 -0600

    Correccion layout de perfil

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -63,19 +63,11 @@
             </li>
             <li>
                 <a href="{{ route('teacher.index') }}">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-archive-fill" viewBox="0 0 16 16">
-                        <path d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z"/>
-                    </svg>
-                    Datos Personales
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('teacher.profile') }}">
                     <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                     </svg>
-                    Perfil
+                    Mi Perfil
                 </a>
             </li>
         </nav>
--- resources/views/teacher-modules/welcome.blade.php
+++ resources/views/teacher-modules/welcome.blade.php
@@ -12,18 +12,12 @@
         <h1>Bienvenido al Sistema de Gestión UDEMEX</h1>
 
         <div class="welcome--module-container">
-            <a href="{{ route('teacher.profile') }}">
+            <a href="{{ route('teacher.index') }}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                 </svg>
-                <h3>Perfil</h3>
-            </a>
-            <a href="{{ route('teacher.index') }}">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-archive-fill" viewBox="0 0 16 16">
-                    <path d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z"/>
-                </svg>
-                <h3>Mis Datos Personales</h3>
+                <h3>Mi perfil</h3>
             </a>
             <a>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-octagon-fill" viewBox="0 0 16 16">
