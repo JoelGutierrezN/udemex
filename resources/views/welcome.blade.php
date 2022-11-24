1bb5684e6ee7f20a8e37450ec72db123f2d37e9b
Eber Palma <al221911577@gmail.com>
2022-11-24 15:43:41 -0600

    Arreglos 24/11/2022

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,45 +34,7 @@
     @include('formulario-perfil.historial-academico')
     {{-- Fin Historial académico --}}
 
-        <style>
-            .modal {
-                display: none; /* Hidden by default */
-                position: fixed; /* Stay in place */
-                z-index: 1; /* Sit on top */
-                left: 0;
-                top: 0;
-                width: 100%; /* Full width */
-                height: 100%; /* Full height */
-                overflow: auto; /* Enable scroll if needed */
-                background: rgb(0,0,0); /* Fallback color */
-                background: rgba(0,0,0,0.4); /* Black w/ opacity */
-            }
-
-                 /* Modal Content/Box */
-            .modal-content {
-                background: #fefefe;
-                margin: auto; /* 15% from the top and centered */
-                padding: 20px;
-                border: 1px solid #888;
-                width: 80%;
-                height: 90vh; /* Could be more or less, depending on screen size */
-            }
-
-                /* The Close Button */
-            .close {
-                color: #aaa;
-                float: right;
-                font-size: 28px;
-                font-weight: bold;
-            }
-
-            .close:hover,
-            .close:focus {
-                color: black;
-                text-decoration: none;
-                cursor: pointer;
-            }
-        </style>
+        
 
         <div id="myModal" class="modal">
 
