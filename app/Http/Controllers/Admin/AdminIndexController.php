606ab717f21bc0443d170f102ae15463ff33cea7
Alfredo Heraz Pérez <al221910309@gmail.com>
2022-12-08 15:54:24 -0600

    entrar_admin

--- app/Http/Controllers/Admin/AdminIndexController.php
+++ app/Http/Controllers/Admin/AdminIndexController.php
@@ -9,7 +9,7 @@ class AdminIndexController extends Controller
 {
     public function __invoke()
     {
-        $num_teachers = User::where('role', 2)->has('informacionAcademica')->count();
-        return view('admin-modules.welcome', compact('num_teachers'));
+        // $num_teachers = User::where('role', 2)->has('informacionAcademica')->count();
+        return view('admin-modules.welcome');
     }
 }
